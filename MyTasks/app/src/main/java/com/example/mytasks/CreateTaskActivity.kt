135e7d9aed1cc8888c2d69a84cfa532340664838
Jjuuko Titus Bazira <jjuukotitus@gmail.com>
2024-04-11 16:28:44 +0300

    modified the create task

--- MyTasks/app/src/main/java/com/example/mytasks/CreateTaskActivity.kt
+++ MyTasks/app/src/main/java/com/example/mytasks/CreateTaskActivity.kt
@@ -2,10 +2,28 @@ package com.example.mytasks
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.mytasks.databinding.ActivityCreateTaskBinding
+import com.example.mytasks.databinding.ActivityMainBinding
 
 class CreateTaskActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityCreateTaskBinding
+    private lateinit var db: TasksDatabaseHelper
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_create_task)
+        binding = ActivityCreateTaskBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        db = TasksDatabaseHelper(this)
+
+        binding.saveButton.setOnClickListener {
+            val title = binding.titleSpace.text.toString()
+            val content = binding.taskDescription.text.toString()
+            val task = Task(0, title, content)
+            db.insertTask(task)
+            finish()
+            Toast.makeText(this,"Task Saved", Toast.LENGTH_SHORT).show()
+        }
     }
 }
