7cf86621ee435aa307e6bfebe60c141ee282becf
Jjuuko Titus Bazira <jjuukotitus@gmail.com>
2024-04-25 00:39:18 +0300

    we can create the tasks, edit and delete them

--- MyTasks/app/src/main/java/com/example/mytasks/MainActivity.kt
+++ MyTasks/app/src/main/java/com/example/mytasks/MainActivity.kt
@@ -3,20 +3,35 @@ package com.example.mytasks
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.mytasks.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var db: TasksDatabaseHelper
+    private lateinit var tasksAdapter: TasksAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        db = TasksDatabaseHelper(this)
+        tasksAdapter = TasksAdapter(db.getALlTasks(),this)
+
+        binding.tasksRecyclerView.layoutManager = LinearLayoutManager(this)
+        binding.tasksRecyclerView.adapter = tasksAdapter
+
         binding.addButton.setOnClickListener {
             val intent = Intent(this,CreateTaskActivity::class.java)
             startActivity(intent)
         }
     }
+
+    override fun onResume() {
+        super.onResume()
+        tasksAdapter.refreshData(db.getALlTasks())
+    }
+
 }
